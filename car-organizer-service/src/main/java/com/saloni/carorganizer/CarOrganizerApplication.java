5e1e77483d2a7ddb18f21a672fa9bcefa04474a4
arat3091 <Newgitl2086$>
2020-02-15 12:39:00 +0100

    first commit to create project structure

---
+++ car-organizer-service/src/main/java/com/saloni/carorganizer/CarOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.saloni.carorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarOrganizerApplication.class, args);
+	}
+
+}
