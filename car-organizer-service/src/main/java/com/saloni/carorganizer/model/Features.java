822e1e65d4e7e6de80d305eeb00e7a86d0861a1d
arat3091 <Newgitl2086$>
2020-02-16 22:56:56 +0100

    update model

--- car-organizer-service/src/main/java/com/saloni/carorganizer/model/Features.java
+++ car-organizer-service/src/main/java/com/saloni/carorganizer/model/Features.java
@@ -2,6 +2,8 @@ package com.saloni.carorganizer.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,16 +12,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Builder
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
 public class Features {
 
   @Id
@@ -31,7 +32,4 @@ public class Features {
 
   @Column
   private BigDecimal cost;
-
-  @ManyToMany(mappedBy = "features")
-  private Set<Model> models = new HashSet<>();
 }
